21472225fdb67ecddc0c10fc57e66b699b2a12e7
kaynanDev <kaynanldev@gmail.com>
2026-04-28 17:12:40 -0300

    feat: página de Not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div
+      className="bg-slate-900 text-slate-200 min-h-95 flex flex-col items-center justify-center gap-4
+     p-8 rounded-xl"
+    >
+      <h1 className="text-7xl font-extrabold">404</h1>
+      <p>Erro 404 - Essa página não existe</p>
+    </div>
+  );
+}
