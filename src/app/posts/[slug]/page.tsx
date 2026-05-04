5346a95c7032ddd5555bd4734b6e0c19adedf6d0
kaynanDev <kaynanldev@gmail.com>
2026-05-04 18:53:24 -0300

    feat: Buscar post pelo slug

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,19 @@
+import api from "@/src/service/api";
+import { notFound } from "next/navigation";
+
 interface SlugProps {
   params: Promise<{ slug: string }>;
 }
 
 export default async function SlugPage({ params }: SlugProps) {
-  const resolvedSlug = await params;
-  return <h1>Rota dinâmica: {resolvedSlug.slug} </h1>;
+  const { slug } = await params;
+
+  try {
+    const response = await api.get(`/posts?slug=${slug}`);
+    const firstPost = response.data[0];
+    const post = firstPost;
+    return <h1>Rota dinâmica: {post.title}</h1>;
+  } catch {
+    notFound();
+  }
 }
