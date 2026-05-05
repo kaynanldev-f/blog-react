7d6258f3bb47fcc1751b6b3e265d134dfcba3287
kaynanDev <kaynanldev@gmail.com>
2026-05-05 17:41:33 -0300

    feat: Função generateMetadata

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,23 @@
 import api from "@/src/service/api";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 interface SlugProps {
   params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: SlugProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const response = await api.get(`/posts?slug=${slug}`);
+  const post = response.data[0];
+  console.log(post);
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
 }
 
 export default async function SlugPage({ params }: SlugProps) {
@@ -10,8 +25,7 @@ export default async function SlugPage({ params }: SlugProps) {
 
   try {
     const response = await api.get(`/posts?slug=${slug}`);
-    const firstPost = response.data[0];
-    const post = firstPost;
+    const post = response.data[0];
     return <h1>Rota dinâmica: {post.title}</h1>;
   } catch {
     notFound();
